91076da0e6b3929038e2ad896d27f0aa127302c8
GENTRIX08 <aluochgentrix008@gmail.com>
2026-02-11 10:51:51 +0300

    program to add,subtract,divide,multiply

---
+++ python-basics/number.py
@@ -0,0 +1,16 @@
+# Name : Gentrix Anyango
+# Date : 11/02/2026
+
+# program to add,subtract,divide,multiply
+
+first_number = (int)input("Enter the first number"))
+second_number =(int)input("Enter the second number"))
+
+
+sum_numbers = first_number + second_number
+difference_numbers = first_number - second_number
+quotient_numbers = first_number / second_number
+product_numbers = first_number * second_number
+
+
+print(f"The sum of the numbers")
