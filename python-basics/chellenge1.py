3194507e3eaeafee3cfb307af6eb5fe1ddd0cc7b
GENTRIX08 <aluochgentrix008@gmail.com>
2026-02-11 09:17:39 +0300

    first commit

---
+++ python-basics/chellenge1.py
@@ -0,0 +1,9 @@
+university = "JKUAT"
+grade = "A"
+favourite_course = "Architecture"
+cluster_points = "80 points"
+
+print(f"My favourite university is JKUAT")
+print(f"My grade is A")
+print(f"My favourite course is Architecture")
+print(f"My cluster points are 80 points")
---
+++ python-basics/hello.py
@@ -0,0 +1,14 @@
+# This is a comment
+print("Hello from Gentrix")
+# city
+print("I am from Nairobi")
+# my hobbies
+print("I like to read novels,watch tv,doomscroll")
+
+age =  18
+height = 1.25
+weight =  75
+
+print(f" I am {age} years old")
+print(f" I am also{height} metres")
+print(f"and weigh {weight} kgs")
