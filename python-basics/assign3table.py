d0818e9d1d23469f6c347ee61080dceb0d88d014
GENTRIX08 <aluochgentrix008@gmail.com>
2026-02-17 10:38:18 +0300

    break,arithmetics,output

---
+++ python-basics/assign3table.py
@@ -0,0 +1,9 @@
+#Name : Gentrix Anyango
+# Date : 16/02/2026
+# Program to calculate trigonometric ratio in a table
+
+import math
+print("______________________________________________________________________")
+print(f"| x  |  sin x   | cos x       |     tan x            |")
+for x in range(-180,210,30):
+    print(f"|  {x}  |  {math.sin(-180)} |  {math.cos(x)} ")
---
+++ python-basics/output_formatting.py
@@ -0,0 +1,28 @@
+#Name : Gentrix Anyango
+# Date : 17/02/2026
+# Program to format the output in different styles
+
+name = "Gentrix Anyango"
+
+weight = 50 # weight in kgs
+
+fav_team = "Liverpool"
+
+height = 126.86 # height in cms
+
+# 1. Format using printf(f"{}")
+
+print(f"My name is {name} and I weigh {weight} in kgs")
+
+# using f string
+msg = f"My name is {name} and I support {fav_team}"
+print(msg)
+
+# 3 using {} and .format()
+print("My name is{0} and I am {1} cms tall". format(name,height))
+
+# 4 using output specifiers %s -strings
+
+import math
+print("The value of pi is approximately %5.3f")
+print("I support %s" %fav_team)
