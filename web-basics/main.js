aa1c458b36980e151d42e282899e1616d93c1e5e
GENTRIX08 <aluochgentrix008@gmail.com>
2026-03-31 08:45:24 +0300

    web basics

---
+++ web-basics/main.js
@@ -0,0 +1,19 @@
+ function message(){
+alert("Hello from Gentrix");
+
+ }
+
+var fnumber,snumber,sumnumber
+ function add(fnumber,snumber){
+    sumnumber = fnumber + snumber;
+    return sumnumber;
+ }
+
+ function changeText()
+ {
+     var btn = document.getElementById("myBtn");
+     var span = getElementById("output");
+     var textbox = document.getElementById("textbox");
+
+     textbox.style.color = "Green"
+ }
